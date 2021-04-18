6e10ed3ab5cf16d9d467c9f32608d3f13f67d162
Igor Garbuz <kinetikapotencia@gmail.com>
2021-04-18 18:38:41 +0300

    fix proj structure

---
+++ Src/Ui/KinetonCarsLog.ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KinetonCarsLog.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
